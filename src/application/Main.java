65f44299919881e8ff8febd727189ee4745162b1
MatheusHBMelo <matheushbmelo@gmail.com>
2023-01-11 10:27:17 -0300

    Refatoração 2.1

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,15 +14,12 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		try {
-			Scanner sc = new Scanner(System.in);
-
+		try (Scanner sc = new Scanner(System.in)) {
 			System.out.print("Digite o CPF que deseja validar: ");
 			String cpf = sc.nextLine();
 
 			ValidatorCpf.validarCPF(cpf);
-		}
-		catch (NumberFormatException e){
+		} catch (NumberFormatException e) {
 			System.out.println("ERRO: Digite apenas números!");
 		}
 	}
